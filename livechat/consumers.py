2d2f14e6a22d7f2ff0cc7a7db7a921b8640b07ef
Yogesh <yogesh@tribute.co>
2025-05-28 15:15:48 +0530

    railway

--- livechat/consumers.py
+++ livechat/consumers.py
@@ -26,11 +26,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.close()
             return
 
-        
+
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-       
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
